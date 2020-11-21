c2049a9fb1c64dde6a0520c5b547ae692e2bfa5d
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-21 12:08:00 -0500

    Hello

--- Hello/hello.py
+++ Hello/hello.py
@@ -2,12 +2,12 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 
-kivy.require('1.9.0')
+kivy.require('1.11.1')
 
 
 class MainApp(App):
     def build(self):
-        return Label(text='Hola Kiby')
+        return Label(text='Hola Kibyy', font_size=72)
 
 if __name__ == '__main__':
     MainApp().run()
