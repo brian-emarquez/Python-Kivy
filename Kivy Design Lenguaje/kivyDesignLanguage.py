e79abd6ecbdc43fd9eb1b1fbe9f07b51dc170e58
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-26 10:58:19 -0500

    feat Kivy Design Languaje
    Kivy Design Languaje
    
    resolve:
    see also:

---
+++ Kivy Design Lenguaje/kivyDesignLanguage.py
@@ -0,0 +1,35 @@
+# Kivy Design Lenguaje
+
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
+
+class MyGridLayout(Widget):
+
+    name = ObjectProperty(None)
+    pizza = ObjectProperty(None)
+    color = ObjectProperty(None)
+
+    def press(self):
+        name = self.name.text
+        pizza =  self.pizza.text
+        color =  self.color.text
+
+        print (f'Hello {name}, you like {pizza} pizza, and your favorite color es {color}')
+        # Clear the input boxes
+        self.name.text= ""
+        self.pizza.text = ""
+        self.color.text = ""
+
+class MyApp(App):
+    def build(self):
+        return MyGridLayout()
+
+if __name__ == '__main__':
+    MyApp().run()
