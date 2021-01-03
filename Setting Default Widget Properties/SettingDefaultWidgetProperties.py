567841d16608c360150ed9ad9d25d86f88cce38a
BrianMarquez3 <brian3marquez@gmail.com>
2021-01-03 12:31:42 -0500

    feat Setting Default Widget Properties  start
    Setting Default Widget Properties start
    
    also:
    see:

---
+++ Setting Default Widget Properties/SettingDefaultWidgetProperties.py
@@ -0,0 +1,27 @@
+#############################################
+# Setting Default Widget Properties
+#############################################
+
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+
+
+Builder.load_file('box.kv')
+
+class MyLayout(Widget):
+    pass
+
+
+class AwersomeApp(App): #my.kv
+    def build(self):
+        return MyLayout()
+
+if __name__ == '__main__':
+    AwersomeApp().run()
