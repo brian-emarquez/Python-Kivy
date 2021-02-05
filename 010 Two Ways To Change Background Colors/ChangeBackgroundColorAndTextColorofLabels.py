aea9029cf53c03c0a5f66df2feb0abc6d808ceb7
BrianMarquez3 <brian3marquez@gmail.com>
2021-02-05 16:49:29 -0500

    feat Two Ways To Change Background Colors START # Two Ways To Change Background Colors start
    Reseolver: #
    see also: #

---
+++ 010 Two Ways To Change Background Colors/ChangeBackgroundColorAndTextColorofLabels.py
@@ -0,0 +1,27 @@
+#############################################
+# Change Background Color And Text Color of Labels
+#############################################
+
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+
+
+Builder.load_file('label_color.kv')
+
+class MyLayout(Widget):
+    pass
+
+
+class AwersomeApp(App): #my.kv
+    def build(self):
+        return MyLayout()
+
+if __name__ == '__main__':
+    AwersomeApp().run()
