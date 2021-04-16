eb0a81922d66a1a857fa376799c01446fca30f40
BrianMarquez3 <brian3marquez@gmail.com>
2021-04-16 18:33:29 -0500

    Update Virt

--- 041 Creating A Login Screen With KivyMD/ColorThemes.py
+++ 041 Creating A Login Screen With KivyMD/ColorThemes.py
@@ -18,9 +18,12 @@ from kivymd.app import MDApp
 class MainApp(MDApp): 
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "BlueGray"
+        self.theme_cls.primary_palette = "DeepPurple"
         self.theme_cls.accent_palette = "Red"
         return Builder.load_file('how.kv')
 
+#'Red', 'Pink', 'Purple', 'DeepPurple',
+#'Indigo', 'Blue', 'LightBlue', 'Cyan',
+#'Teal', 'Gren', 'LightGreen', 'Lime'
 
 MainApp().run()
