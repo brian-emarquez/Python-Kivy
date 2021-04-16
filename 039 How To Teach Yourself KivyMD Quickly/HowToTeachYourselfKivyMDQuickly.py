a5196c03e242114cb31db428814ccbb2f51f4c9f
BrianMarquez3 <brian3marquez@gmail.com>
2021-04-16 17:43:05 -0500

    Update

--- 039 How To Teach Yourself KivyMD Quickly/HowToTeachYourselfKivyMDQuickly.py
+++ 039 How To Teach Yourself KivyMD Quickly/HowToTeachYourselfKivyMDQuickly.py
@@ -13,16 +13,17 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
+from kivymd.app import MDApp
 
 Builder.load_file('how.kv')
 
 class MyLayout(Widget):
     pass
 
-class AwersomeApp(App): #my.kv
+class AwersomeApp(MDApp): #my.kv
     def build(self):
         return MyLayout()
-        
+
 if __name__ == '__main__':
     AwersomeApp().run()
     
