215c083a6f6da44d5e22fce045397c49287844e0
BrianMarquez3 <brian3marquez@gmail.com>
2021-04-16 17:55:37 -0500

    Using Color Themes For KivyMD

---
+++ 040 Using Color Themes For KivyMD/ColorThemes.py
@@ -0,0 +1,29 @@
+
+#Autor: Brian E
+#Fecha: 16 Abril 2021
+#Contacto: brian@mail.com
+#Descrption: How To Teach Yourself KivyMD Quickly
+
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
+
+class MainApp(MDApp): 
+    def build(self):
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.accent_palette = "Blue"
+        return Builder.load.file('how.kv')
+
+#'Red', 'Pink', 'Purple', 'DeepPurple',
+#'Indigo', 'Blue', 'LightBlue', 'Cyan',
+#'Teal', 'Gren', 'LightGreen', 'Lime'
+
+MainApp().run()
