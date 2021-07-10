3a30a7f3ec45800143d99c80bb8e0391322636eb
brian3marquez <brian3marquez@gmail.com>
2021-07-10 14:24:41 -0500

    044 Speed Dial Button

---
+++ 044 Speed Dial Button Menu With KivyMD/speed.py
@@ -0,0 +1,26 @@
+
+#Autor: Brian E
+#Fecha: 10 Julio 2021
+#Contacto: brian@mail.com
+#Descrption: Speed Dial Button Menu With KivyMD
+
+from kivy.lang import Builder
+from kivyMD.app import MDApp
+
+class MainApp(MDApp): 
+
+    data = {
+        "language-python":"Python",
+        "language-ruby":"Ruby",
+        "language-javascript":"JS",
+
+    }
+
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        return Builder.load_file('how.kv')
+
+
+
+MainApp().run()
